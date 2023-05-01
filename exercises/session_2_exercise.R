00b11ff6b1be970e85cf72cb4ea8217ec2f371fe
sofiawo <sofia.p.wolf@gmail.com>
2023-05-01 21:44:43 +0200

    Exercise 2, 2,1 runif() command

---
+++ exercises/session_2_exercise.R
@@ -0,0 +1,28 @@
+
+# 2.1 
+?runif
+?mean
+?round
+x <- runif(100, 1, 100) #generates n (100) random deviates between min (1) and max(100)
+mean_x <- round(mean(x), 2) #calculates arithmetic mean and rounds until 2 decimal places
+mean_x
+
+x <- runif(100, 1, 100)
+mean_x <- round(mean(x), 2)
+mean_x
+
+x <- runif(100, 1, 100)
+mean_x <- round(mean(x), 2)
+mean_x
+
+x <- runif(100, 1, 100)
+mean_x <- round(mean(x), 2)
+mean_x
+#produces random values, differ each time which leads to different mean values
+
+
+x <- scale(runif(100, 1, 100))
+mean_x <- round(mean(x), 2)
+mean_x
+
+#2.2
